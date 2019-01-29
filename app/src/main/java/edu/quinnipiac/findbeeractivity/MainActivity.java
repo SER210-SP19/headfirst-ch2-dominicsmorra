88c9caebf3df63d29787a30121fa3d40f797806a
Dominic Smorra <dominicsmorra@gmail.com>
2019-01-28 19:17:45 -0500

    third commit

--- app/src/main/java/edu/quinnipiac/findbeeractivity/MainActivity.java
+++ app/src/main/java/edu/quinnipiac/findbeeractivity/MainActivity.java
@@ -2,12 +2,26 @@ package edu.quinnipiac.findbeeractivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickFindBeer(View view) {
+        TextView brands = (TextView) findViewById(R.id.brands);
+        brands.setText("Gottle of geer");
+        Spinner color = (Spinner) findViewById(R.id.color);
+        String.valueOf(color.getSelectedItem());
+
+    }
+
+
 }
